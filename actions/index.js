c7dff242b3295fe5cf9e95fb12c0014d79433a44
codeNesta <38645206+codeNesta@users.noreply.github.com>
2019-10-04 23:30:19 -0700

    Update index.js

--- actions/index.js
+++ actions/index.js
@@ -2,12 +2,27 @@ import firebase from '../fb'
 
 export function getBlogs(){
     return(dispatch) => {
-       firebase.database().ref('/blogs').on('value', snapshot => {
-                dispatch({
-                    type:"BLOGS_FETCH",
-                    payload:snapshot.val()
-                })
-       })
+
+        dispatch({
+            type:"BLOGS_LOADING_STATUS",
+            payload:true
+        })
+
+        firebase.database().ref('/blogs').on('value', snapshot => {
+                    dispatch({
+                        type:"BLOGS_FETCH",
+                        payload:snapshot.val()
+                    })
+
+                    dispatch({
+                        type:"BLOGS_LOADING_STATUS",
+                        payload:false
+                    })
+              
+        })
+
+       
+                               
     }
 }
 
