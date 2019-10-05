000b321c0baf36b942e7828281a0bb6c62abcf9a
codeNesta <38645206+codeNesta@users.noreply.github.com>
2019-10-04 23:35:26 -0700

    Update Blogs.js

--- components/Blogs.js
+++ components/Blogs.js
@@ -14,33 +14,37 @@ class Blogs extends Component {
  }
 
     render() {
-      
+        
         return (
             <View style={styles.container}>
-                <FlatList style={{width:'100%'}} 
-                           data={this.props.listOfBlogs}
-                           keyExtractor={(item) => item.key}
-                           showsVerticalScrollIndicator={false}
-                           renderItem={({item}) => {
-                               return (
-                                   <View style={{elevation:8, marginBottom:15,borderRadius:15, backgroundColor:'#575FCF', padding:20}}>
-                                       <Text style={{fontSize:28,  fontWeight:'bold', color:'#fff', marginBottom: 10,}} > {item.title}</Text>
-                                       <Text style={{fontSize:20, lineHeight:30, color:'#fff'}}>{item.content}</Text>
-                                        <View style={{flexDirection:'row', justifyContent:'flex-end', marginTop:25}}>
-                                            <TouchableHighlight onPress={() => this.props.navigation.navigate('Edit',{...item})}>
-                                                    <View style={{marginRight:15}}>
-                                                        <Icon size={30} color="white" name="edit" />
-                                                    </View>
-                                            </TouchableHighlight>   
-                                            <TouchableHighlight onPress={() =>this.props.deleteBlog(item.key)} >
-                                                    <View>
-                                                        <Icon size={30} color="white" name="close" />
-                                                    </View>
-                                            </TouchableHighlight>   
-                                        </View>
+
+                {
+                     this.props.loadingReducer ? <Text>Loading Please Wait</Text> : <FlatList style={{width:'100%'}} 
+                     data={this.props.listOfBlogs}
+                     keyExtractor={(item) => item.key}
+                     showsVerticalScrollIndicator={false}
+                     renderItem={({item}) => {
+                         return (
+                             <View style={{elevation:8, marginBottom:15,borderRadius:15, backgroundColor:'#575FCF', padding:20}}>
+                                 <Text style={{fontSize:28,  fontWeight:'bold', color:'#fff', marginBottom: 10,}} > {item.title}</Text>
+                                 <Text style={{fontSize:20, lineHeight:30, color:'#fff'}}>{item.content}</Text>
+                                  <View style={{flexDirection:'row', justifyContent:'flex-end', marginTop:25}}>
+                                      <TouchableHighlight onPress={() => this.props.navigation.navigate('Edit',{...item})}>
+                                              <View style={{marginRight:15}}>
+                                                  <Icon size={30} color="white" name="edit" />
+                                              </View>
+                                      </TouchableHighlight>   
+                                      <TouchableHighlight onPress={() =>this.props.deleteBlog(item.key)} >
+                                              <View>
+                                                  <Icon size={30} color="white" name="close" />
+                                              </View>
+                                      </TouchableHighlight>   
                                   </View>
-                               )
-                           }} />
+                            </View>
+                         )
+                     }} />
+                }
+               
             </View>
         );
     }
@@ -67,7 +71,9 @@ function mapStateToProps(state){
     })
 
     return {
-        listOfBlogs
+        listOfBlogs,
+        loadingReducer:state.loadingReducer.loadingReducer
+        
     }
 }
 
